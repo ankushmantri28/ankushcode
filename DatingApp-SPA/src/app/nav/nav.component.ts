37f7c9501029e055517928b035e4844e68b0d601
ankush <ankush28.am@gmail.com>
2020-02-16 17:37:51 +0530

    register_users_code

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/Auth.service';
+import { error } from 'util';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+model: any = {};
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+login() {
+  //console.log(this.model);
+  this.authService.login(this.model).subscribe(next => {
+    console.log('logged in successfully');
+  }, error => {
+    console.log("failed to login");
+  });
+}
+loggedIn() {
+const token = localStorage.getItem('token');
+return !!token;
+}
+logout() {
+  localStorage.removeItem('token');
+  console.log('logged out');
+}
+}

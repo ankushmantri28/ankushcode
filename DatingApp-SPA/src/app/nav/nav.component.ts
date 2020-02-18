ab282349039116c44f7548c3ae12c9f394de3a10
ankush <ankush28.am@gmail.com>
2020-02-18 10:22:01 +0530

    Exception Hadeling angular and core

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -18,7 +18,7 @@ login() {
   this.authService.login(this.model).subscribe(next => {
     console.log('logged in successfully');
   }, error => {
-    console.log("failed to login");
+    console.log(error);
   });
 }
 loggedIn() {
